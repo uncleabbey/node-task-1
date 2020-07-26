bb24c79c1913b5c039a77414fd6c3367e9095895
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-07-26 15:27:03 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const http = require('http')
 
-const app = http.createServer((req, res) => {
-  res.end('Hello World')
-});
+const server = http.createServer(function(req, res) {
+  if (req.method == 'POST') {
+    console.log('POST')
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString(); // convert Buffer to string
+    });
+    req.on('end', () => {
+        var data = JSON.parse(body)
+        res.end(`Hello ${data.name}, Welcome to WeJapa Internships`);
+    });
+  } else {
+    console.log('GET')
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end('Hello World, Welcome to WeJapa Internships')
+  }
+})
 
-
-app.listen(5000, '127.0.0.1')
-
-console.log('Server is running on port 3000')
-
-
+const port = 3000
+const host = '127.0.0.1'
+server.listen(port, host)
+console.log(`Listening at http://${host}:${port}`)
